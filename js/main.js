4515fdcb67e62c6d0e3d09a9c39c711b50eef080
Daniele Moro <moro.daniele@gmail.com>
2018-05-14 19:00:25 -0600

    Restructured code to be more OOP

--- js/main.js
+++ js/main.js
@@ -1,63 +1,50 @@
 "use strict";
+let game = new Game(100);
+game.run();
 
-let running = true;
-let speed = 100;
-run();
+function Game (speed) {
+  this.speed = speed;
+  this.running = true;
+  this.canvas = new Canvas(50);
+  this.board = new Board(this.canvas.xMax, this.canvas.yMax);
+  this.canvas.drawBoard(this.board);
 
-async function run() {
-  let board = new Board(50);
-  board.drawCanvas();
-
-  //Keep running
-  // noinspection InfiniteLoopJS
-  for (let turns = 0; ; turns++) {
-    await sleep(speed);
-    if (running) {
-      console.log(turns);
-      for (let x = 0; x < board.xWidth; x++) {
-        for (let y = 0; y < board.yWidth; y++) {
-          //game of life
-          let neighbors = countNeighbors(board, x, y);
-          //kill lonely or overcrowded cells
-          if (neighbors < 2 || neighbors > 3) {
-            board.setCell(x, y, 0);
-          }
-          //create new cells
-          else if (neighbors === 3) {
-            board.setCell(x, y, 1);
-          }
+  this.gameOfLifeRules = function(board){
+    for (let x = 0; x < board.xMax; x++) {
+      for (let y = 0; y < board.yMax; y++) {
+        //game of life
+        let neighbors = board.countNeighbors(x, y);
+        //kill lonely or overcrowded cells
+        if (neighbors < 2 || neighbors > 3) {
+          board.setCell(x, y, 0);
+        }
+        //create new cells
+        else if (neighbors === 3) {
+          board.setCell(x, y, 1);
         }
       }
     }
-  }
+  };
+
+  this.run = async function(){
+    //Keep running
+    // noinspection InfiniteLoopJS
+    for (let turns = 0; ; turns++) {
+      await sleep(this.speed);
+      this.canvas.drawBoard(this.board);
+      if (this.running) {
+        console.log(turns);
+        this.gameOfLifeRules(this.board);
+      }
+    }
+  };
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function countNeighbors(board, x, y){
-  let totalCount = 0;
-  totalCount += x>0 && y>0 ?  board.getCell(x-1, y-1) : 0; //topleft
-  totalCount += y>0 ?  board.getCell(x, y-1) : 0; //top
-  totalCount += x<(board.xWidth-1) && y>0 ?  board.getCell(x+1, y-1) : 0; //topRight
-  totalCount += x>0 ?  board.getCell(x-1, y) : 0; //left
-  totalCount += x<(board.xWidth-1) ?  board.getCell(x+1, y) : 0; //right
-  totalCount += x>0 && y<(board.yWidth-1) ? board.getCell(x-1,y+1) : 0; //bottomRight
-  totalCount += y<(board.yWidth-1) ?  board.getCell(x,y+1) : 0; //bottom
-  totalCount += x<(board.xWidth-1) && y<(board.yWidth-1) ?  board.getCell(x+1,y+1) : 0; //bottomRight
-  return totalCount;
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function Board (yWidth) {
+function Board (xMax, yMax) {
   this.setupBoard = function() {
-    for (let i = 0; i < this.xWidth; i++) {
+    for (let i = 0; i < this.xMax; i++) {
       let cols = [];
-      for (let j = 0; j < this.yWidth; ++j) {
+      for (let j = 0; j < this.yMax; ++j) {
         cols[j] = 0;
       }
       this.board[i] = cols;
@@ -67,8 +54,8 @@ function Board (yWidth) {
   };
 
   this.clearBoard = function() {
-    for (let x = 0; x < this.xWidth; x++) {
-      for (let y = 0; y < this.yWidth; y++) {
+    for (let x = 0; x < this.xMax; x++) {
+      for (let y = 0; y < this.yMax; y++) {
         this.setCell(x,y,0);
       }
     }
@@ -80,8 +67,45 @@ function Board (yWidth) {
 
   this.setCell = function (x, y, value) {
     this.board[x][y] = value;
-    this.drawCell(x,y,value)
   };
+
+  this.countNeighbors = function(x, y){
+    let totalCount = 0;
+    totalCount += x>0 && y>0 ?  this.getCell(x-1, y-1) : 0; //topleft
+    totalCount += y>0 ?  this.getCell(x, y-1) : 0; //top
+    totalCount += x<(this.xMax-1) && y>0 ?  this.getCell(x+1, y-1) : 0; //topRight
+    totalCount += x>0 ?  this.getCell(x-1, y) : 0; //left
+    totalCount += x<(this.xMax-1) ?  this.getCell(x+1, y) : 0; //right
+    totalCount += x>0 && y<(this.yMax-1) ? this.getCell(x-1,y+1) : 0; //bottomRight
+    totalCount += y<(this.yMax-1) ?  this.getCell(x,y+1) : 0; //bottom
+    totalCount += x<(this.xMax-1) && y<(this.yMax-1) ?  this.getCell(x+1,y+1) : 0; //bottomRight
+    return totalCount;
+  };
+
+  this.randPopulate = function() {
+    //set random start cells
+    let startCells = (this.xMax*this.yMax)/10;
+    for (let i = 0; i < startCells; i++) {
+      this.setCell(getRandomInt(this.xMax-1), getRandomInt(this.yMax-1), 1)
+    }
+  };
+
+  this.xMax = xMax;
+  this.yMax = yMax;
+  this.board = [];
+  this.setupBoard();
+}
+
+function Canvas (yMax) {
+  this.canvas = document.getElementById("myCanvas");
+  this.cvs = this.canvas.getContext("2d");
+  this.cvs.canvas.height = window.innerHeight - 20;
+  this.cvs.canvas.width = window.innerWidth - 20;
+  this.yMax = yMax;
+  this.cellHeight = this.cvs.canvas.height / this.yMax;
+  this.cellWidth = this.cellHeight;
+  this.xMax = Math.trunc(this.cvs.canvas.width / this.cellWidth);
+  let self = this;
 
   this.drawCell = function(x, y, val){
     //draw surrounding white square
@@ -94,10 +118,10 @@ function Board (yWidth) {
     this.cvs.fillRect((x * this.cellWidth) + 1, (y * this.cellHeight ) + 1, this.cellWidth - 2, this.cellHeight - 2);
   };
 
-  this.drawCanvas = function(){
-    for(let i = 0; i < this.xWidth; i++) {
-      for(let j = 0; j < this.yWidth; j++) {
-        this.drawCell(i, j, this.getCell(i,j))
+  this.drawBoard = function(board){
+    for(let i = 0; i < board.xMax; i++) {
+      for(let j = 0; j < board.yMax; j++) {
+        this.drawCell(i, j, board.getCell(i,j))
       }
     }
   };
@@ -110,34 +134,13 @@ function Board (yWidth) {
     };
   };
 
-  this.randPopulate = function() {
-    //set random start cells
-    let startCells = (this.xWidth*this.yWidth)/10;
-    for (let i = 0; i < startCells; i++) {
-      this.setCell(getRandomInt(this.xWidth-1), getRandomInt(this.yWidth-1), 1)
-    }
-  };
-
-  this.canvas = document.getElementById("myCanvas");
-  this.cvs = this.canvas.getContext("2d");
-  this.board = [];
-
-  this.cvs.canvas.height = window.innerHeight - 20;
-  this.cvs.canvas.width = window.innerWidth - 20;
-  this.yWidth = yWidth;
-  this.cellHeight = this.cvs.canvas.height / this.yWidth;
-  // noinspection JSSuspiciousNameCombination
-  this.cellWidth = this.cellHeight;
-  this.xWidth = Math.trunc(this.cvs.canvas.width / this.cellWidth);
-  let self = this;
-
   //Handle mouse events
   this.canvas.addEventListener('mousemove', function(evt) {
     if(self.mouseDown){
       let mousePos = self.getMousePos(evt);
       let xCell = Math.trunc(mousePos.x / self.cellWidth);
       let yCell = Math.trunc(mousePos.y / self.cellHeight);
-      self.setCell(xCell, yCell, 1);
+      game.board.setCell(xCell, yCell, 1);
     }
   }, false);
 
@@ -155,31 +158,37 @@ function Board (yWidth) {
   document.body.onkeyup = function(e){
     switch(e.key){
       case " ":
-        running = !running;
+        game.running = !game.running;
         break;
 
       case "c":
-        self.clearBoard();
+        game.board.clearBoard();
         break;
 
       case "p":
-        self.randPopulate();
+        game.board.randPopulate();
         break;
 
       case "ArrowLeft":
-        speed += 100;
+        game.speed += 100;
         break;
 
       case "ArrowRight":
-        speed -= 100;
+        game.speed -= 100;
         break;
 
       default:
         return;
     }
   };
+}
 
-  this.setupBoard();
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
